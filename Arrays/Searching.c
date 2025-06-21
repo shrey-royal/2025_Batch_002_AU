34cef0e3aefff98dafb4c5de9e9abb30f2e12bcf
Shrey K <srkkadia@gmail.com>
2025-06-21 12:06:34 +0530

    Radix Sort

--- Arrays/Searching.c
+++ Arrays/Searching.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
+
+void swap(int* first, int* second) {
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+void bubbleSort(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < size-1-i; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
 
 void printArray(int *arr, int size) {
     printf("\nArray elements: { ");
--- Arrays/Sorting.c
+++ Arrays/Sorting.c
@@ -115,6 +115,49 @@ void quickSort(int arr[], int left, int right) {
     }
 }
 
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void countingSort(int arr[], int n, int place) {
+    int output[n];
+    int count[10] = {0};
+
+    for (int i = 0; i < n; i++) {
+        int digit = (arr[i] / place) % 10;
+        count[digit]++;
+    }
+    
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i-1];
+    }
+
+    for (int i = n-1; i >= 0; i--) {
+        int digit = (arr[i] / place) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(int arr[], int n) {
+    int max = getMax(arr, n);
+
+    for (int place = 1; max / place > 0; place *= 10) {
+        countingSort(arr, n, place);
+    }
+}
+
+
 int main() {
     int size;
 
@@ -126,7 +169,7 @@ int main() {
     int arr[size];
 
     for (int i = 0; i < size; i++) {
-        arr[i] = (rand() % 100) - 49;
+        arr[i] = (rand() % 100);
     }
 
     printf("\nBefore Sorting: ");
@@ -136,7 +179,8 @@ int main() {
     // selectionSort(arr, size);
     // insertionSort(arr, size);
     // mergeSort(arr, 0, size-1);
-    quickSort(arr, 0, size-1);
+    // quickSort(arr, 0, size-1);
+    radixSort(arr, size);
 
     printf("\nAfter Sorting: ");
     printArray(arr, size);
