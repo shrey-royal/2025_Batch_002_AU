4c811a64a0f8166ed97c9bad5b0a8f72008ad198
Shrey K <srkkadia@gmail.com>
2025-06-26 13:22:43 +0530

    Simple Queue

--- Linear DS/Queues/Queue.c
+++ Linear DS/Queues/Queue.c
@@ -18,6 +18,25 @@ void enqueue(struct Queue *q, int value) {
     }
 }
 
+void dequeue(struct Queue *q) {
+    if (q->front == -1) printf("\nQueue is empty");
+    else {
+        printf("\nDequeue element: %d", q->items[q->front]);
+        if (q->front == q->rear) q->front = q->rear = -1;
+        else q->front++;
+    }
+}
+
+int peekFront(struct Queue *q) {
+    if (q->front == -1) printf("\nQueue is empty");
+    else return q->items[q->front];
+}
+
+int peekRear(struct Queue *q) {
+    if (q->rear == -1) printf("\nQueue is empty");
+    else return q->items[q->rear];
+}
+
 void display(struct Queue *q) {
     if (q->rear == -1) printf("\nQueue is empty!");
     else {
@@ -37,7 +56,18 @@ int main() {
     enqueue(&q, 10);
     enqueue(&q, 20);
     enqueue(&q, 30);
+    enqueue(&q, 40);
+    enqueue(&q, 50);
+    enqueue(&q, 60);
+    enqueue(&q, 70);
+    enqueue(&q, 80);
+    enqueue(&q, 90);
+    enqueue(&q, 200);
+    dequeue(&q);
+    enqueue(&q, 30);
     display(&q);
+    printf("\npf: %d", peekFront(&q));
+    printf("\npr: %d", peekRear(&q));
     
     return 0;
 }
