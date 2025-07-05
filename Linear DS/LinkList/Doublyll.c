c0ffba81e4f9381c18261086c650e03067c71a77
Shrey K <srkkadia@gmail.com>
2025-07-05 10:47:09 +0530

    insertAtEnd

--- Linear DS/LinkList/Doublyll.c
+++ Linear DS/LinkList/Doublyll.c
@@ -22,20 +22,55 @@ node* createNode(int data) {
     return newNode;
 }
 
-void insertAtBeginning(node* head, int value) {
+void insertAtBeginning(node** head, int value) {
     node* newNode = createNode(value);
 
-    if (head != NULL) {
-        newNode->next = head;
-        head->prev = newNode;
+    if (*head != NULL) {
+        newNode->next = *head;
+        (*head)->prev = newNode;
     }
-    return newNode;
+    *head = newNode;
+}
+
+void insertAtEnd(node** head, int value) {
+    node* newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        node* temp = *head;
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+}
+
+void insertAtPosition(node** head, int value) {
+    //
+}
+
+void printList(node* head) {
+    printf("\nList > \tNULL <-> ");
+    while(head != NULL) {
+        printf("%d <-> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n\n");
 }
 
 int main() {
     node* head = NULL;
 
-    insertAtBeginning(&head, 10);
+    // insertAtBeginning(&head, 10);
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtEnd(&head, 40);
+    insertAtEnd(&head, 50);
+    insertAtEnd(&head, 60);
+
+    printList(head);
 
     return 0;
 }
