d9987abc6518042dacc893c0111d21ec3bd01157
Shrey K <srkkadia@gmail.com>
2025-07-27 12:54:57 +0530

    AVL

--- Non-Linear DS/AVL.c
+++ Non-Linear DS/AVL.c
@@ -24,6 +24,95 @@ node* createNode(int data) {
     return newNode;
 }
 
+int getBalance(node* n) {
+    return n ? getHeight(n->left) - getHeight(n->right) : 0;
+}
+
+node* rightRotate(node* unBalancedNode) {
+    node* newRoot = unBalancedNode->left;
+    node* rightSubtreeOfNewRoot = newRoot->right;
+
+    // perform rotation
+    newRoot->right = unBalancedNode;
+    unBalancedNode->left = rightSubtreeOfNewRoot;
+
+    // Update heights
+    unBalancedNode->height = max(getHeight(unBalancedNode->left), getHeight(unBalancedNode->right)) + 1;
+    newRoot->height = max(getHeight(newRoot->left), getHeight(newRoot->right)) + 1;
+
+    return newRoot;
+}
+
+node* leftRotate(node* unBalancedNode) {
+    node* newRoot = unBalancedNode->right;
+    node* leftSubtreeOfNewRoot = newRoot->left;
+
+    // perform rotation
+    newRoot->left = unBalancedNode;
+    unBalancedNode->right = leftSubtreeOfNewRoot;
+
+    // Update heights
+    unBalancedNode->height = max(getHeight(unBalancedNode->left), getHeight(unBalancedNode->right)) + 1;
+    newRoot->height = max(getHeight(newRoot->left), getHeight(newRoot->right)) + 1;
+
+    return newRoot;
+}
+
+node* insert(node* root, int value) {
+    if(root == NULL) return createNode(value);
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if(value > root->data) {
+        root->right = insert(root->right, value);
+    } else {
+        return root;
+    }
+
+    // Update heights
+    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+
+    // check balance
+    int balance = getBalance(root);
+
+    if (balance > 1 && value < root->left->data) {  // LL
+        return rightRotate(root);
+    }
+    if (balance < -1 && value > root->right->data) {    // RR
+        return leftRotate(root);
+    }
+    if (balance > 1 && value > root->left->data) {  // LR
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    if (balance < -1 && value < root->right->data) {    // RL
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+void inorder(node* root) {
+    if(root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void printTree(node* root, int space) {
+    if (root == NULL) {
+        return;
+    }
+    space += 4;
+    printTree(root->right, space);
+    for (int i = 4; i < space; i++) {
+        printf(" ");
+    }
+    printf("%d\n", root->data);
+    printTree(root->left, space);
+}
+
 int main() {
     node* root = NULL;
 
@@ -36,6 +125,9 @@ int main() {
 
     printf("Inorder Traversal of AVL Tree: ");
     inorder(root);
+    printf("\n");
+
+    printTree(root, 0);
     
     return 0;
 }
